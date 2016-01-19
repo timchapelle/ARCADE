478c965b1ac40c05ea6b157e0cad8ee632e189e3
timchapelle <horace22@gmail.com>
2016-01-19 01:16:14 +0100

    Ajout Fct_TirerUnMot et Jeu vs CPU + Fct_Menu !

--- Pendu/pendu.sh
+++ Pendu/pendu.sh
@@ -5,62 +5,90 @@
 # coAuteur : Matthieu Schmit
 # v1.0 : 18/01/2016
 
+Fct_Menu() {
+	clear
+	potence=0
+	listelettres=''
+	
+	echo -e "$JAUNE" "\t\t\t ___________"
+	echo -e "$JAUNE" "\t\t\t|           |"
+	echo -e "$JAUNE" "\t\t\t| $ROUGE LE PENDU $JAUNE|"
+	echo -e "$JAUNE" "\t\t\t|___________|""$NORMAL\n\n"
+	echo -e "$BLEU\t _____________\t\t\t ______________"
+	echo -e "$BLEU\t|             |\t\t\t|              |" 
+	echo -e "$BLEU\t| $VERT (J)1 vs J2$BLEU |\t\t\t| $VERT J1 vs (C)PU$BLEU |"
+	echo -e "$BLEU\t|_____________|\t\t\t|______________|"
+	
+	read -n 1 choixJeu
+	case $choixJeu in 
+		'j'|'J') Fct_Jeu
+			;;
+		'c'|'C') Fct_TirerUnMot
+			;;
+		*) echo -e "$ROUGE Alors, ... C ou J, J ou C, à toi de choisir"
+		   read -n 1 choixJeu
+	esac
+}
+
 Fct_Jeu() {
 	clear
 	potence=0
 	listelettres=''
 		
-	echo -e "$ROUGE" "\t\t\tLE PENDU"
-	echo -e "$JAUNE" "\t\t\t--------""$NORMAL\n\n"
-		
 	echo -e "$BLEU Joueur 1, quel est ton nom ? $NORMAL"
 	read Joueur1
-	echo -e "$BLEU Joueur 2, quel est ton nom ? $NORMAL"
-	read Joueur2
-	
-	echo -e "\n $VERT Qui va devoir deviner le mot ?\n\n"
-	choixJ=3
-	while [[ $choixJ -ne 1 && $choixJ -ne 2 ]] ; do 
-		echo -e "$JAUNE $Joueur1 (1)\t ou \t $Joueur2 ? (2) \n\n"
-		read -n 1 choixJ
-	done
-	
-	statut='ko'
-	while [ $statut != 'ok' ] ; do
-	
-		if [ $choixJ -eq 1 ] ; then 
-		echo -e "$VERT $Joueur2, rentre le mot à deviner (3-10 lettres)\n\n$CACHE"
-			devineur=$Joueur1
-			cacheur=$Joueur2
-		else 
-			echo -e "$VERT $Joueur1, rentre le mot à deviner (3-10 lettres)\n\n$CACHE"
-			devineur=$Joueur2
-			cacheur=$Joueur1
-		fi
+	if [[ $choixJeu = 'j' || $choixJeu = 'J' ]] ; then
+		echo -e "$BLEU Joueur 2, quel est ton nom ? $NORMAL"
+		read Joueur2
+		echo -e "\n $VERT Qui va devoir deviner le mot ?\n\n"
 		
-	read MotAdeviner
-	Fct_EnleverAccent
-	Fct_TestAlpha
-	longueur=${#MotAdeviner}
+		choixJ=3
 		
-		if [ $longueur -lt 3 ] ; then 
-			echo -e "$DEF""$ROUGE Ce mot est trop court ! Entrez un mot de min. 3 caractères $NORMAL"
-			statut='ko'
-		elif  [ $longueur -gt 10 ] ; then
-			echo -e "$DEF""$ROUGE Ce mot est trop long ! Entrez un mot contenant max. 10 caractères $NORMAL"
-			statut='ko'
-		else 
-			echo -e "$DEF"
-			clear
-			echo -e "$BLEU\n\t\t\tC'est parti, on va donc jouer avec ${#MotAdeviner} lettres"
-			statut='ok'
-			Fct_ProposerLettre
-		fi
-	done 
+		while [[ $choixJ -ne 1 && $choixJ -ne 2 ]] ; do 
+			echo -e "$JAUNE $Joueur1 (1)\t ou \t $Joueur2 ? (2) \n\n"
+			read -n 1 choixJ
+		done
+	
+		statut='ko'
+		while [ $statut != 'ok' ] ; do
+	
+			if [ $choixJ -eq 1 ] ; then 
+				echo -e "$VERT $Joueur2, rentre le mot à deviner (3-10 lettres)\n\n$CACHE"
+				devineur=$Joueur1
+				cacheur=$Joueur2
+			else 
+				echo -e "$VERT $Joueur1, rentre le mot à deviner (3-10 lettres)\n\n$CACHE"
+				devineur=$Joueur2
+				cacheur=$Joueur1
+			fi
+		
+			read MotAdeviner
+		
+			longueur=${#MotAdeviner}
+		
+			if [ $longueur -lt 3 ] ; then 
+				echo -e "$DEF""$ROUGE Ce mot est trop court ! Entrez un mot de min. 3 caractères $NORMAL"
+				statut='ko'
+			elif  [ $longueur -gt 10 ] ; then
+				echo -e "$DEF""$ROUGE Ce mot est trop long ! Entrez un mot contenant max. 10 caractères $NORMAL"
+				statut='ko'
+			else 
+				echo -e "$DEF"
+				clear
+								statut='ok'
+			fi
+		done
+	else Joueur2='BrainMaster'
+		 Fct_TirerUnMot
+	fi
+	Fct_TestAlpha	
+	Fct_ProposerLettre
 	}
 	
 Fct_ProposerLettre() {
+	Fct_EnleverAccent
 	
+	echo -e "$BLEU\n\t\t\tC'est parti, on va donc jouer avec ${#MotAdeviner} lettres"
 	while [ $potence -lt 10 ]; do
 	echo -e "$BLEU Tapez une lettre : \n $NORMAL"
 	read -n 1 lettre
@@ -120,14 +148,15 @@ Fct_Scores() {
 	
 	echo -e "Vainqueur : $gagnant\nPerdant : $perdant\nMot à deviner : $MotAdeviner \n\n" >> .pendu
 	
-	echo -e "$VERT""Voulez-vous rejouer ? O/N"
+	echo -e "$VERT""Voulez-vous rejouer ? O/N\nOu retourner au (M)enu ?"
 	read -n 1 choix
 	
 	case $choix in 
 		'o'|'O') Fct_Jeu
 					;;
-			  *) exit 0
+		'm'|'M') Fct_Menu
 					;;
+			  *) exit 0
 	esac
 	}
 
@@ -172,6 +201,13 @@ Fct_TestAlpha () {
 	done
 }
 
+Fct_TirerUnMot () {
+	
+	nb1=$( wc -l $liste | cut -f1 -d' ' )
+	n=$(( RANDOM % $nb1 + 1 ))
+	MotAdeviner=$( sed -n ${n}p $liste )
+	Fct_ProposerLettre
+}
 #Couleurs:	
 VERT="\\033[1;32m"
 NORMAL="\\033[0;39m"
@@ -183,6 +219,7 @@ DEF="\\033[0;0m"
 
 fichierScores=".pendu"
 dossierDessins=".Dessins/"
+liste=".liste"
 
-Fct_Jeu
+Fct_Menu
 
